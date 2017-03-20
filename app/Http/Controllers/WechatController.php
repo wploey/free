aa077afca2eee107404739b02d462efc24a49052
wploey <wploey@gmail.com>
2017-03-20 15:45:44 +0800

    'tok'

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -3,26 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use EasyWeChat\Foundation\Application;
 
 class WechatController extends Controller
 {
-    public function token(){
-        $options = [
-            'debug'  => env('WX_DEBUG'),
-            'app_id' => env('WX_APP_ID'),
-            'secret' => env('WX_SECRET'),
-            'token'  => env('WX_TOKEN'),
-            // 'aes_key' => null, // 可选
-            'log' => [
-                'level' => 'debug',
-                'file'  => '/tmp/easywechat.log', // XXX: 绝对路径！！！！
-            ],
-            //...
-        ];
-
-        $app = new Application($options);
-        $response = $app->server->serve();
-        return $response;
-    }
+    //
 }
