4c1d2996b400876baab5853fee5f12dbd42f0f59
wploey <wploey@gmail.com>
2017-03-20 14:47:02 +0800

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Free xxx
+                    Laravel
                 </div>
 
                 <div class="links">
